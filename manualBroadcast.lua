2de1b292297f9430737c768c8dc4daf4be37cc4c
RockBrackenshield <ethan.in.virginia@gmail.com>
2015-04-14 10:31:34 -0400

    Update manualBroadcast.lua

--- manualBroadcast.lua
+++ manualBroadcast.lua
@@ -5,16 +5,33 @@ if not component.isAvailable("modem") then
  io.stderr:write("A modem is required to use this program, preferably from a wireless network card.")
  return
 end
+if not component.isAvailable("access_point") then
+ io.stderr:write("An access point is required to use this program. It allows for great wireless transmissions")
+ return
+end
  
+local ap = component.access_point
 local m = component.modem
  
-print("Please input on which port you would like to broadcast: ")
+if not m.isWireless() then
+ print("Sorry, this program only really does anything useful if you have a wireless network card installed")
+ return
+end
+ 
+print("Please input on which port you would like to broadcast (number): ")
 local portstr = io.read()
-print("Now, please input the message you would like to send: ")
+print("Now, please input the message you would like to send (string): ")
 local msg = io.read()
+print("Lastly, how far would you like this broadcast to go? (number): ")
+local strrange = io.read()
  
 print("Reading input, preparing message...")
 local portnum = tonumber(portstr)
+print("Message prepared, setting desired broadcast range...")
+local range = tonumber(strrange)
+m.setStrength(range)
+ap.setStrength(range)
+print("Range set to: " .. range)
  
 print("Input read, checking port status...")
  
